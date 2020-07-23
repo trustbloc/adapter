5043393893e529d9b84b1b6d6820b69db7a4c602
Rolson Quadras <rolson.quadras@securekey.com>
2020-07-23 17:39:09 -0400

    fix: [Issuer Adapter] Remove user button clicks during Wallet Connect
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

--- cmd/issuer-adapter-vue/src/pages/WalletConnect.vue
+++ cmd/issuer-adapter-vue/src/pages/WalletConnect.vue
@@ -11,15 +11,7 @@ SPDX-License-Identifier: Apache-2.0
         <h2 class="blue bg-aqua text-black">Connect Wallet to the Issuer (DIDComm)</h2>
         <br/>
 
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                v-on:click="connectWallet" v-if="!connectWalletSuccess">Connect Wallet
-        </button>
-
         <p v-if="connectWalletSuccess">Wallet Connected Successfully.
-            <a style="color:green;" :href="redirectURL">Click
-                Here</a>
-            to return to Issuer Page.
-        </p>
         <p style="color:red;">{{ connectWalletErr }}</p>
 
     </div>
@@ -32,70 +24,72 @@ SPDX-License-Identifier: Apache-2.0
             return {
                 connectWalletSuccess: false,
                 connectWalletErr: null,
-                redirectURL: null,
             }
         },
-        methods: {
-            connectWallet: async function () {
-                const invitationUrl = "/issuer/didcomm/chapi/request?txnID=" + this.$route.query.txnID
+        created: async function () {
+            const invitationUrl = "/issuer/didcomm/chapi/request?txnID=" + this.$route.query.txnID
 
-                let chapiRequest
-                await this.$http.get(invitationUrl).then(
-                    resp => {
-                        chapiRequest = resp.data
-                    },
-                    err => {
-                        console.error(`failed to retrieve didcomm invitation: url=${invitationUrl} err=${err}`)
-                    }
-                )
-
-                if (chapiRequest === undefined) {
-                    this.connectWalletErr = "Failed to Connect Wallet."
-
-                    return;
+            let chapiRequest
+            await this.$http.get(invitationUrl).then(
+                resp => {
+                    chapiRequest = resp.data
+                },
+                err => {
+                    console.error(`failed to retrieve didcomm invitation: url=${invitationUrl} err=${err}`)
                 }
+            )
 
-                await this.$polyfill.loadOnce()
+            if (chapiRequest === undefined) {
+                this.connectWalletErr = "Failed to Connect to Wallet."
 
-                const connectionRequest = {
-                    web: {
-                        VerifiablePresentation: chapiRequest
-                    }
-                };
-
-                console.log("Sending credential query", connectionRequest)
-
-                const result = await navigator.credentials.get(connectionRequest);
-                console.log("DIDComm connection webcredential response:", result.data)
-
-                if (!result || !result.data) {
-                    console.error("Failed to Connect Wallet - no response")
-
-                    this.connectWalletErr = "Failed to Connect Wallet."
-                    return;
-                }
-
-                const validateUrl = "/connect/validate?txnID=" + this.$route.query.txnID
-                await this.$http.post(validateUrl, {walletResp: result.data}).then(
-                    resp => {
-                        if (resp.status !== 200) {
-                            console.error(`failed to validate wallet response: url=${validateUrl} status=${resp.status} err=${resp.data}`)
-
-                            this.connectWalletErr = "Failed to Connect Wallet. " + resp.data.errMessage
-                            return
-                        }
-
-                        this.connectWalletSuccess = true
-                        this.redirectURL = resp.data.redirectURL
-
-                        console.log(`wallet connected successfully; redirectURL=${resp.data.redirectURL}`)
-                    },
-                    err => {
-                        console.error(`failed to validate wallet response: url=${validateUrl} err=${err}`)
-                        this.connectWalletErr = "Failed to Connect Wallet."
-                    }
-                )
+                return;
             }
+
+            await this.$polyfill.loadOnce()
+
+            const connectionRequest = {
+                web: {
+                    VerifiablePresentation: chapiRequest
+                }
+            };
+
+            console.log("chapi request : ", JSON.stringify(connectionRequest))
+
+            const result = await navigator.credentials.get(connectionRequest);
+
+            console.log("chapi response : ", JSON.stringify(result.data))
+
+            if (!result || !result.data) {
+                console.error("Failed to Connect Wallet - no response")
+                this.connectWalletErr = "Failed to Connect Wallet."
+
+                return;
+            }
+
+            const validateUrl = "/connect/validate?txnID=" + this.$route.query.txnID
+            await this.$http.post(validateUrl, {walletResp: result.data}).then(
+                resp => {
+                    if (resp.status !== 200) {
+                        console.error(`failed to validate wallet response: url=${validateUrl} status=${resp.status} err=${resp.data}`)
+
+                        this.connectWalletErr = "Failed to Connect to Wallet. " + resp.data.errMessage
+                        return
+                    }
+
+                    this.connectWalletSuccess = true
+                    const redirectURL = resp.data.redirectURL
+
+                    console.log(`wallet connected successfully; redirectURL=${redirectURL}`)
+
+                    window.location.href = redirectURL
+                },
+                err => {
+                    console.error(`failed to validate wallet response: url=${validateUrl} err=${err}`)
+                    this.connectWalletErr = "Failed to Connect to Wallet."
+
+                    return;
+                }
+            )
         }
     }
 </script>
