562a71bd88f60e00396941ae7c1b53d59606b774
Filip Burlacu <filip.burlacu@securekey.com>
2021-03-10 10:25:57 -0500

    chore: use trustbloc/wallet-js-client@0.1.6 instead of cicd snapshot
    Signed-off-by: Filip Burlacu <filip.burlacu@securekey.com>

--- cmd/issuer-adapter-vue/vue.config.js
+++ cmd/issuer-adapter-vue/vue.config.js
@@ -5,8 +5,9 @@ SPDX-License-Identifier: Apache-2.0
 */
 
 const path = require('path');
-const isSnapshot = require('./package.json').dependencies.hasOwnProperty("@trustbloc-cicd/wallet-js-client")
-const wallet_client = (isSnapshot) ? "@trustbloc-cicd/wallet-js-client" : "@trustbloc/wallet-js-client"
+// const isSnapshot = require('./package.json').dependencies.hasOwnProperty("@trustbloc-cicd/wallet-js-client")
+// const wallet_client = (isSnapshot) ? "@trustbloc-cicd/wallet-js-client" : "@trustbloc/wallet-js-client"
+const wallet_client = "@trustbloc/wallet-js-client"
 
 module.exports = {
     publicPath: "/ui",
